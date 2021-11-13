7498f1121472e32bfbcc9569589a2ac1c9e7fe59
yoonseo <yo26on@soongsil.ac.kr>
2021-11-14 06:57:17 +0900

    [#3] TabBar Setting

--- DaangnMarket/DaangnMarket/Source/Screen/Base/TabBarController.swift
+++ DaangnMarket/DaangnMarket/Source/Screen/Base/TabBarController.swift
@@ -14,24 +14,53 @@ final class TabBarController: UITabBarController {
         makeTabBarItems()
         setTabBarAppearance()
     }
+
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        guard let itemView = item.value(forKey: "view") as? UIView else { return }
+        let itemAnimator = UIViewPropertyAnimator(duration: 0.1, dampingRatio: 0.5) {
+            itemView.transform = CGAffineTransform.identity.scaledBy(x: 1.2, y: 1.2)
+        }
+        itemAnimator.addAnimations({itemView.transform = .identity }, delayFactor: 0.5)
+        itemAnimator.startAnimation()
+   }
 }
 
 extension TabBarController {
     private func makeTabBarItems() {
         let home1View = Home1ViewController()
-        home1View.tabBarItem = UITabBarItem()
+        home1View.tabBarItem = UITabBarItem(
+            title: "홈",
+            image: Image.homeIcon,
+            selectedImage: Image.homeIcon
+        )
 
         let home2View = Home2ViewController()
-        home2View.tabBarItem = UITabBarItem()
+        home2View.tabBarItem = UITabBarItem(
+            title: "동네생활",
+            image: Image.neighborIcon,
+            selectedImage: Image.neighborIcon
+        )
 
         let myTownView = MyTownViewController()
-        myTownView.tabBarItem = UITabBarItem()
+        myTownView.tabBarItem = UITabBarItem(
+            title: "내 근처",
+            image: Image.locationIcon,
+            selectedImage: Image.locationIcon
+        )
 
         let chattingView = ChattingViewController()
-        chattingView.tabBarItem = UITabBarItem()
+        chattingView.tabBarItem = UITabBarItem(
+            title: "채팅",
+            image: Image.chatsIcon,
+            selectedImage: Image.chatsIcon
+        )
 
         let myPageView = MyPageViewController()
-        chattingView.tabBarItem = UITabBarItem()
+        myPageView.tabBarItem = UITabBarItem(
+            title: "나의 당근",
+            image: Image.myPageIcon,
+            selectedImage: Image.myPageIcon
+        )
 
         viewControllers = [
             home1View,
@@ -47,6 +76,11 @@ extension TabBarController {
         UITabBar.appearance().shadowImage = UIImage()
         UITabBar.appearance().backgroundImage = UIImage()
         UITabBar.appearance().backgroundColor = UIColor.white
-        UITabBar.appearance().layer.borderWidth = 0.0
+        UITabBar.appearance().layer.borderColor = Color.daangnGray1.cgColor
+        UITabBar.appearance().layer.borderWidth = 0.3
+        UITabBarItem.appearance().setTitleTextAttributes([.font: UIFont.systemFont(ofSize: 10, weight: .regular)],
+                                                         for: .normal)
+        UITabBar.appearance().tintColor = Color.daangnBlack1
+        UITabBar.appearance().unselectedItemTintColor = Color.daangnBlack1
     }
 }
