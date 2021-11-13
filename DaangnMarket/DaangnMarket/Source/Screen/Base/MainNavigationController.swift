bbd1e0a4ed1b3a1cad666ce6367d8098385b1b9d
yoonseo <yo26on@soongsil.ac.kr>
2021-11-14 07:06:32 +0900

    [#3] NavigationBar Setting

--- DaangnMarket/DaangnMarket/Source/Screen/Base/MainNavigationController.swift
+++ DaangnMarket/DaangnMarket/Source/Screen/Base/MainNavigationController.swift
@@ -19,11 +19,23 @@ class MainNavigationController: BaseNavigationController {
         $0.customView = label
     }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    private lazy var downArrowButtonItem = UIBarButtonItem().then {
+        $0.customView = makeBarButtonItem(image: Image.downArrowIcon, tag: 0)
     }
 
+    private lazy var searchButtonItem = UIBarButtonItem().then {
+        $0.customView = makeBarButtonItem(image: Image.searchIcon, tag: 1)
+    }
+
+    private lazy var categoryButtonItem = UIBarButtonItem().then {
+        $0.customView = makeBarButtonItem(image: Image.categoryIcon, tag: 2)
+    }
+
+    private lazy var alarmButtonItem = UIBarButtonItem().then {
+        $0.customView = makeBarButtonItem(image: Image.alarmIcon, tag: 3)
+    }
+
+    // MARK: - Life Cycle
     override init(rootViewController: UIViewController) {
         super.init(rootViewController: rootViewController)
         initNavigationItem(navigationItem: rootViewController.navigationItem)
@@ -32,12 +44,36 @@ class MainNavigationController: BaseNavigationController {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
 }
 
 extension MainNavigationController {
+    // MARK: - init NavigationItem
     private func initNavigationItem(navigationItem: UINavigationItem?) {
         let spacing = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
-        spacing.width = 12
-        navigationItem?.leftBarButtonItem = logoItem
+        spacing.width = 6
+        navigationItem?.leftBarButtonItems = [logoItem, spacing, downArrowButtonItem]
+        navigationItem?.rightBarButtonItems = [alarmButtonItem, spacing, categoryButtonItem, spacing, searchButtonItem]
     }
+
+    // MARK: - make BarButtonItem
+    private func makeBarButtonItem(image: UIImage, tag: Int) -> UIButton {
+        let button = UIButton(type: .custom)
+        button.tag = tag
+        button.setImage(image, for: .normal)
+        button.frame = CGRect(x: 0, y: 0, width: 32, height: 32)
+        button.addTarget(self, action: #selector(buttonDidTapped(_:)), for: .touchUpInside)
+        return button
+    }
+}
+
+// MARK: - @objc
+extension MainNavigationController {
+    @objc
+    private func buttonDidTapped(_ sender: UIButton) {}
 }
--- DaangnMarket/DaangnMarket/Source/Support/AppDelegate.swift
+++ DaangnMarket/DaangnMarket/Source/Support/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = MainNavigationController(rootViewController: TabBarController())
-        window?.backgroundColor = Color.daangnGray5
+        window?.backgroundColor = Color.daangnWhite
         window?.makeKeyAndVisible()
         return true
     }
