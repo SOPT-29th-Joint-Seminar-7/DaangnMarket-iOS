d3c3d00eed31394a1a686bc7f58582507bd1f8cb
yoonseo <yo26on@soongsil.ac.kr>
2021-11-15 00:23:37 +0900

    [#5] extension 추가
    ### Description
    Layout 구현에 필요한 extension 추가
    
    이슈티켓 #5

---
+++ DaangnMarket/DaangnMarket/Source/Extension/UIView+.swift
@@ -0,0 +1,16 @@
+//
+//  UIView+.swift
+//  DaangnMarket
+//
+//  Created by 김윤서 on 2021/11/15.
+//
+
+import UIKit.UIView
+
+extension UIView{
+    func addSubviews(_ views:UIView...){
+        for view in views {
+            self.addSubview(view)
+        }
+    }
+}
