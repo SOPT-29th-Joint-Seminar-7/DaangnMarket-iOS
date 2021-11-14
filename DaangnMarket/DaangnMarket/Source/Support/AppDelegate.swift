ba786f1afca24efe9b1fde4ada36a6434ae63924
yoonseo <yo26on@soongsil.ac.kr>
2021-11-15 00:09:36 +0900

    [#5] WritingViewController NavigationBar세팅
    ### Description
    WritingViewController에서 사용되는 NavigationBar를 만들었습니다
    
    🎫 이슈 티켓: #5

--- DaangnMarket/DaangnMarket/Source/Support/AppDelegate.swift
+++ DaangnMarket/DaangnMarket/Source/Support/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = MainNavigationController(rootViewController: TabBarController())
+        window?.rootViewController = BaseNavigationController(rootViewController: WritingViewController())
         window?.backgroundColor = Color.daangnWhite
         window?.makeKeyAndVisible()
         return true
