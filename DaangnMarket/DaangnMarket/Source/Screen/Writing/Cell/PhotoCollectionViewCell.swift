d3c785029608da9a8212fe19f91629841ed6ee28
yoonseo <yo26on@soongsil.ac.kr>
2021-11-15 01:15:23 +0900

    [#5] PhotoTableViewCell 구현
    ### Description
    PhotoTableViewCell 구현
    
    이슈티켓 #5

---
+++ DaangnMarket/DaangnMarket/Source/Screen/Writing/Cell/PhotoCollectionViewCell.swift
@@ -0,0 +1,67 @@
+//
+//  PhotoCollectionViewCell.swift
+//  DaangnMarket
+//
+//  Created by 김윤서 on 2021/11/15.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+
+    private let vStackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.spacing = 5
+        $0.distribution = .fillEqually
+    }
+
+    private let imageView = UIImageView().then {
+        $0.image = Image.photoIcon
+        $0.contentMode = .scaleAspectFit
+    }
+
+    private let label = UILabel().then {
+        $0.text = "0/10"
+        $0.textColor = Color.daangnGray3
+        $0.font = .systemFont(ofSize: 12, weight: .regular)
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        round()
+        setLayouts()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension PhotoCollectionViewCell {
+    private func round() {
+        layer.cornerRadius = 3
+        layer.borderWidth = 1
+        layer.borderColor = Color.daangnGray3.cgColor
+        clipsToBounds = true
+    }
+}
+
+extension PhotoCollectionViewCell {
+    func setLayouts() {
+        setViewHierarchies()
+        setConstraints()
+    }
+
+    func setViewHierarchies() {
+        contentView.addSubview(vStackView)
+        vStackView.addArrangedSubview(imageView)
+        vStackView.addArrangedSubview(label)
+    }
+
+    func setConstraints() {
+        vStackView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+
+        }
+    }
+}
---
+++ DaangnMarket/DaangnMarket/Source/Screen/Writing/Cell/PhotoTableViewCell.swift
@@ -0,0 +1,91 @@
+//
+//  PhotoTableViewCell.swift
+//  DaangnMarket
+//
+//  Created by 김윤서 on 2021/11/15.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class PhotoTableViewCell: UITableViewCell {
+
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .white
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.register(cell: PhotoCollectionViewCell.self)
+        return collectionView
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setLayouts()
+        setDelegation()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension PhotoTableViewCell {
+    private func setDelegation() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+}
+
+extension PhotoTableViewCell: UICollectionViewDelegate {
+}
+
+extension PhotoTableViewCell: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: PhotoCollectionViewCell = collectionView.dequeueReusableCell(forIndexPath: indexPath)
+        return cell
+    }
+}
+
+extension PhotoTableViewCell: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 60, height: 60)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 31, left: 16, bottom: 18, right: 0)
+    }
+
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+}
+
+extension PhotoTableViewCell {
+    private func setLayouts() {
+        setViewHierarchies()
+        setConstraints()
+    }
+
+    private func setViewHierarchies() {
+        contentView.addSubview(collectionView)
+    }
+
+    private func setConstraints() {
+        collectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
