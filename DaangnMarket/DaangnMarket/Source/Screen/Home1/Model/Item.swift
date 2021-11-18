c93b8a513a412f347da174709f80c0af425bffab
suyeon <rlatndus9911@naver.com>
2021-11-19 02:31:14 +0900

    [#16] ItemTableViewCell 구현

---
+++ DaangnMarket/DaangnMarket/Source/Screen/Home1/Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  DaangnMarket
+//
+//  Created by 김수연 on 2021/11/18.
+//
+
+import Foundation
+import UIKit
+
+struct Item {
+    let itemImage: String
+    let itemTitle: String
+    let location: String
+    let uploadDate: String
+    let itemPrice: String
+}
